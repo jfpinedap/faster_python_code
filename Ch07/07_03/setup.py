a7346488ca826f7af69029f648bc1fbd246e0183
Juan Pineda <Juan_Pineda@epam.com>
2023-02-14 23:13:50 -0500

    commands to build C and Cpython code -- Also for execute pytest-benchmark test

--- Ch07/07_03/setup.py
+++ Ch07/07_03/setup.py
@@ -4,5 +4,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize('cysqrt.pyx'),
+    ext_modules=cythonize("cysqrt.pyx"),
 )
+
+# Command to build: python setup.py build_ext -i
