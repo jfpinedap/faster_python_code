a493c3f8874db4cf0ce20713bf4de5f415da60e8
Juan Pineda <Juan_Pineda@epam.com>
2023-02-14 23:12:35 -0500

    commands to build C and Cpython code -- Also for execute pytest-benchmark test

--- Ch08/08_03/fib.py
+++ Ch08/08_03/fib.py
@@ -1,6 +1,9 @@
 """Simple fibonacci"""
 
+from functools import lru_cache
 
+
+@lru_cache()
 def fib(n):
     """Return n'th fibonacci number"""
     if n < 2:
--- Ch08/08_03/test_fib.py
+++ Ch08/08_03/test_fib.py
@@ -4,3 +4,10 @@ from fib import fib
 def test_fib(benchmark):
     result = benchmark(fib, 30)
     assert result == 1346269
+
+
+
+# Commands to perform tests with pytest-benchmark: 
+# pytest --benchmark-autosave
+# pytest --benchmark-autosave --benchmark-compare
+
